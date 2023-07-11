6cdca0f6d0942f0e29e1862626c6eec1ed555d92
Alice Pote <alice.writes.wrongs@gmail.com>
2023-07-11 12:44:51 -0500

    starting to comment out the <custom-checkbox> component

--- internals.js
+++ internals.js
@@ -1,47 +1,55 @@
-// with element internals
+// This is a pretty basic 'checkbox' component using `ElementInternals` which
+// is minimalist to the point of being essentially useless (it doesn't offer
+// any functionality beyond what you get from just `<input type="checkbox">`
+// and in fact it merely wraps such an input!)
+//
+// However, it's useful as a simple example which is documented with a bunch of
+// comments to explain how it all works.
+//
+// The component has:
+//
+// 1. Attributes for common things allowing you to easily inspect it's state in
+//    the browser.
+// 2. Boilerplate for echoing the value of its `<input>` element out to the
+//    form it resides within.
+// 3. Validation (where it's valid when checked and invalid otherwise). Since
+//    this is using the `ElementInternals` validation APIs it should prevent
+//    submission of the form as long as it's invalid.
 class CustomCheckbox extends HTMLElement {
+  // we have to set this to true! See
+  // https://developer.mozilla.org/en-US/docs/Web/API/HTMLElement/attachInternals#examples
   static formAssociated = true;
 
   constructor() {
     super();
+    // we also need to call this method and store the return value
     this.internals_ = this.attachInternals();
-    this.value_ = false;
+    this.__value = false;
   }
 
   inputEl () {
     return this.shadowRoot.querySelector("input");
   }
 
-  get value() {
-    return this.value_;
-  }
   set value(v) {
-    this.value_ = !!v;
-    this.inputEl().checked = !!v;
-    this.internals_.setFormValue(this.name, this.value_);
+    this.__value = !!v;
+    this.inputEl().checked = this.__value;
+    this.internals_.setFormValue(this.__value);
   }
-  get form() {
-    return this.internals_.form;
-  }
-  get name() {
-    return this.getAttribute('name');
-  }
-  get type() {
-    return this.localName;
-  }
-  get validity() {
-    return this.internals_.validity;
-  }
-  get validationMessage() {
-    return this.internals_.validationMessage;
-  }
-  get willValidate() {
-    return this.internals_.willValidate;
-  }
+  get value() { return this.__value; }
+
+  // a bunch of getters for various things (mostly for convenience)
+  get form() { return this.internals_.form; }
+  get name() { return this.getAttribute('name'); }
+  get type() { return this.localName; }
+  get validity() { return this.internals_.validity; }
+  get validationMessage() { return this.internals_.validationMessage; }
+  get willValidate() { return this.internals_.willValidate; }
 
   checkValidity() {
     return this.internals_.checkValidity();
   }
+
   reportValidity() {
     return this.internals_.reportValidity();
   }
@@ -55,9 +63,21 @@ class CustomCheckbox extends HTMLElement {
 
     const that = this;
 
+    // This `onchange` handler which we attach to the `<input>` el inside of
+    // our CE's shadow DOM will take care of echoing its value out to the
+    // form.
+    //
+    // Note that we don't need to pass a name or anything here - the browser
+    // will look at the `name` attr on the instance of our CE to figure out
+    // what name to insert in the formdata.
+    //
+    // This is perhaps a bit more 'manual' than I thought this would be, but
+    // there's a lot of flexibility here with this type of imperative interface
+    // to the underlying form. You could easily add, for instance, custom
+    // validation in your `onchange` or `oninput` handler 
     input.onchange = ev => {
-      this.value_ = !this.value_;
-      this.internals_.setFormValue("value", ev.target.value);
+      this.__value = !!ev.target.value;
+      this.internals_.setFormValue(this.value);
     };
 
     // Create some CSS to apply to the shadow DOM
@@ -73,18 +93,3 @@ class CustomCheckbox extends HTMLElement {
 }
 
 window.customElements.define("custom-checkbox", CustomCheckbox);
-
-
-let element = document.createElement("custom-checkbox");
-element.setAttribute("name", "test-name");
-let form = document.createElement("form");
-
-// Append element to form to associate it
-form.appendChild(element);
-
-console.log(element.internals_.form);
-// expected output: <form><custom-checkbox></custom-checkbox></form>
-
-
-const body = document.querySelector("body");
-body.appendChild(form);
